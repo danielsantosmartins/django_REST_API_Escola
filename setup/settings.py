09c1eb0d7221485e8ebb8540633aa06fec9b2c83
Daniel Martins <danielsantosmartins.dev@gmail.com>
2024-07-08 22:03:40 -0300

    Update: incluida CORS na API

--- setup/settings.py
+++ setup/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'escola',
+    "corsheaders",
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -141,3 +143,7 @@ REST_FRAMEWORK = {
     #     'anon': '100/day',
     # }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  
+]
